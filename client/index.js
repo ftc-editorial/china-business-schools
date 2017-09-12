15bea19db54b7b9d3d475349a436b1bdb1de7c40
nilssonpatricia <nilssonpatricia@email.com>
2017-09-12 17:18:42 +0100

    Start adding oTable JS

--- client/index.js
+++ client/index.js
@@ -1,29 +1,5 @@
 import './styles.scss';
 
-/*
-  TODO: delete this comment
+import oTable from 'o-table/main'; // eslint-disable-line
 
-  This file is where you bootstrap your JS code
-  For example import stuff here:
-
-  import {select} from 'd3-selection';
-  import myComponent from './components/my-component';
-
-  Split logical parts of you project into components e.g.
-
-  /client
-    - /components
-        - /component-name
-            - styles.scss
-            - index.js
-            - template.html
-
-  If you want to import some data, just import it like normal:
-
-  import myData from './data.csv';
-
-  `myData` will be a string that you can then parse into an object using, for example, d3.csvParse()
-
-  You can import CSV, TSV, TXT, XML and JSON this way! Note, however, that it will increase your
-  bundle size, which may increase the time to first render in some cases!
-*/
+oTable.init();
