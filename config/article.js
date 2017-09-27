685229dea9be8b828d25c3698c763b80109a99b2
David Blood <davidbjourno@users.noreply.github.com>
2017-09-27 12:08:36 +0100

    URL; copy edits

--- config/article.js
+++ config/article.js
@@ -5,7 +5,7 @@ export default () => ({ // eslint-disable-line
 
   // canonical URL of the published page
   // "$url" get filled in by the ./configure script
-  url: 'https://ig.ft.com/chinese-business-schools',
+  url: 'https://ig.ft.com/greater-china-business-schools',
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
